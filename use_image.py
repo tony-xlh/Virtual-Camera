12e65958430ca994ac939c32456fbb483d0708ab
xulihang <xulihanghai@163.com>
2022-01-20 13:29:14 +0800

    add sys.argv

--- use_image.py
+++ use_image.py
@@ -1,11 +1,21 @@
 import pyvirtualcam
 import cv2
+import sys
 
-frame = cv2.imread("example.jpg")
-width = frame.shape[1]
-height = frame.shape[0]
-cam = pyvirtualcam.Camera(width=width, height=height, fps=20)
-while True:
-    cam.send(frame)
-    cam.sleep_until_next_frame()
+def main(path):
+    frame = cv2.imread(path)
+    width = frame.shape[1]
+    height = frame.shape[0]
+    cam = pyvirtualcam.Camera(width=width, height=height, fps=20)
+    while True:
+        cam.send(frame)
+        cam.sleep_until_next_frame()
+    
+if __name__ == "__main__":
+    path = "example.png"
+    if len(sys.argv) == 2 :
+        path = sys.argv[1]
+    print(path)
+    main(path)
+    
     
--- use_video.py
+++ use_video.py
@@ -1,17 +1,26 @@
 import av
 import pyvirtualcam
+import sys
 
-container = av.open("example.mp4")
-height = container.streams[0].codec_context.coded_height
-width = container.streams[0].codec_context.coded_width
+def main(path):
+    container = av.open(path)
+    height = container.streams[0].codec_context.coded_height
+    width = container.streams[0].codec_context.coded_width
 
-cam = pyvirtualcam.Camera(width=width, height=height, fps=20)
+    cam = pyvirtualcam.Camera(width=width, height=height, fps=20)
 
-while True:
-    container = av.open("example.mp4")
-    stream = container.streams.video[0]
-    for frame in container.decode(stream):
-        frame = frame.to_ndarray(format='bgr24')
-        cam.send(frame)
-        cam.sleep_until_next_frame()
+    while True:
+        container = av.open("example.mp4")
+        stream = container.streams.video[0]
+        for frame in container.decode(stream):
+            frame = frame.to_ndarray(format='bgr24')
+            cam.send(frame)
+            cam.sleep_until_next_frame()
+        
+if __name__ == "__main__":
+    path = "example.mp4"
+    if len(sys.argv) == 2 :
+        path = sys.argv[1]
+    print(path)
+    main(path)
     
