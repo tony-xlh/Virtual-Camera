4f44c7cedb6332b2304a78e7f37d0ebe5bb2c3cb
xulihang <xulihanghai@163.com>
2022-01-20 11:30:29 +0800

    update width and height

--- use_image.py
+++ use_image.py
@@ -1,7 +1,9 @@
 import pyvirtualcam
 import cv2
 
-frame = cv2.imread("example.png")
+frame = cv2.imread("9e4e9ab49713b37ad055e460bfc1f434.jpg")
+width = frame.shape[1]
+height = frame.shape[0]
 cam = pyvirtualcam.Camera(width=width, height=height, fps=20)
 while True:
     cam.send(frame)
